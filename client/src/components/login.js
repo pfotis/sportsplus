0c2fdf9bece6be97dd01ab24fd5361da84e1fb27
pfotis <69961508+pfotis@users.noreply.github.com>
2021-04-22 20:19:04 +1000

    create state to get email and password at sign in and firstname lastname email and password when signup the user

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,34 +1,107 @@
 import React, { Component } from "react";
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
 
 export default class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            password: '',
+            redirectTo: null
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        // console.log('handleSubmit')
+
+        axios
+            .post('/user/login', {
+                username: this.state.username,
+                password: this.state.password
+            })
+            .then(response => {
+                console.log('login response: ')
+                console.log(response)
+                if (response.status === 200) {
+                    // update App.js state
+                    this.props.updateUser({
+                        loggedIn: true,
+                        username: response.data.username
+                    })
+                    // update the state to redirect to home
+                    this.setState({
+                        redirectTo: '/'
+                    })
+                }
+            }).catch(error => {
+                console.log('login error: ')
+                console.log(error);
+
+            })
+    }
+
     render() {
-        return (
-            <form>
+        if (this.state.redirectTo) {
+            return <Redirect to ={{ pathname: this.state.redirectTo }} />
+        }
+        else {
+            return (
+                <form>
 
-                <h3>Log in</h3>
+                    <h3>Log in</h3>
 
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input 
+                            type="email"
+                            name="username"
+                            className="form-control" 
+                            placeholder="Enter email"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
                     </div>
-                </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
-        );
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input 
+                            type="password"
+                            className="form-control"
+                            placeholder="Enter password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button 
+                        type="submit" 
+                        className="btn btn-dark btn-lg btn-block"
+                        onClick={this.handleSubmit}
+                    >Sign in</button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </form>
+            );
+        }
     }
 }
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,37 +1,124 @@
 import React, { Component } from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 export default class SignUp extends Component {
+    constructor() {
+		super()
+		this.state = {
+            firstname: '',
+            lastname: '',
+			username: '',
+			password: '',
+			redirectTo: null
+
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+    handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+    handleSubmit(event) {
+		console.log('sign-up handleSubmit, username: ')
+		console.log(this.state.username)
+		event.preventDefault()
+
+		//request to server to add a new username/password
+		axios.post('/user/', {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+			username: this.state.username,
+			password: this.state.password,
+		})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg && (this.state.password == this.state.confirmPassword)) {
+					console.log('successful signup')
+					this.setState({ //redirect to login page
+						redirectTo: '/login'
+					})
+				} else {
+					console.log('username already taken')
+				}
+			}).catch(error => {
+				console.log('signup error: ')
+				console.log(error)
+
+			})
+	}
+
     render() {
-        return (
-            <form>
-                <h3>Register</h3>
+        if (this.state.redirectTo) {
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        } 
+        else {
+            return (
+                <form>
+                    <h3>Register</h3>
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
+                    <div className="form-group">
+                        <label>First name</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="First name"
+                            name="firstname"
+                            value={this.state.firstname}
+							onChange={this.handleChange}
+                        />
+                    </div>
 
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+                    <div className="form-group">
+                        <label>Last name</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="Last name" 
+                            name="lastname"
+                            value={this.state.lastname}
+							onChange={this.handleChange}
+                        />
+                    </div>
 
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            placeholder="Enter email" 
+                            name="username"
+                            value={this.state.username}
+							onChange={this.handleChange}
+                        />
+                    </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            placeholder="Enter password" 
+                            name="password"
+							value={this.state.password}
+							onChange={this.handleChange}
+                        />
+                    </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">log in?</a>
-                </p>
-            </form>
-        );
+                    <button 
+                        type="submit" 
+                        className="btn btn-dark btn-lg btn-block"
+                        onClick={this.handleSubmit}
+                    >Register</button>
+                    <p className="forgot-password text-right">
+                        Already registered <a href="#">log in?</a>
+                    </p>
+                </form>
+            );
+        }
     }
 }
