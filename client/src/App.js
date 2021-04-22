8370727a3f5fdb8f583e62eea4bce427e53817e1
pfotis <69961508+pfotis@users.noreply.github.com>
2021-04-22 19:39:15 +1000

    create sign in and sign up form

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// import Login from "./components/login";
+// import SignUp from "./components/signup";
+import Home from "./components/home";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Home />
   );
 }
 
